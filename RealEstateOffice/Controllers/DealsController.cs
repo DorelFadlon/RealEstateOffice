c8e5d272fbd952606e96379a1bafd5d9d78e9ed5
Gal JacobSon <Jacobsongal@gmail.com>
2020-06-03 18:36:08 +0300

    view-controller

---
+++ RealEstateOffice/Controllers/DealsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RealEstateOffice.Models;
+
+namespace RealEstateOffice.Controllers
+{
+    public class DealsController : Controller
+    {
+        private OfficeDB db = new OfficeDB();
+
+        // GET: Deals
+        public ActionResult Index()
+        {
+            return View(db.Deals.ToList());
+        }
+
+        // GET: Deals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deal deal = db.Deals.Find(id);
+            if (deal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deal);
+        }
+
+        // GET: Deals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Deals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DealID,PropertyID,CustomerID,BrokerID,Price,Commition,Categorty")] Deal deal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Deals.Add(deal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(deal);
+        }
+
+        // GET: Deals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deal deal = db.Deals.Find(id);
+            if (deal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deal);
+        }
+
+        // POST: Deals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DealID,PropertyID,CustomerID,BrokerID,Price,Commition,Categorty")] Deal deal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(deal);
+        }
+
+        // GET: Deals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deal deal = db.Deals.Find(id);
+            if (deal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deal);
+        }
+
+        // POST: Deals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Deal deal = db.Deals.Find(id);
+            db.Deals.Remove(deal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
