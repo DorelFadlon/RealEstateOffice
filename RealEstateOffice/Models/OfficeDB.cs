896d15fe982ee586de7006347678a3900eb332a0
Gal JacobSon <Jacobsongal@gmail.com>
2020-06-05 14:23:58 +0300

    X

--- RealEstateOffice/Models/OfficeDB.cs
+++ RealEstateOffice/Models/OfficeDB.cs
@@ -5,13 +5,10 @@ namespace RealEstateOffice.Models
 {
     public class OfficeDB : DbContext
     {
-        public OfficeDB()
-        {
-
-        }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Broker> Brokers { get; set; }
         public DbSet<Property> Properties { get; set; }
         public DbSet<Deal> Deals { get; set; }
+
     }
 }
